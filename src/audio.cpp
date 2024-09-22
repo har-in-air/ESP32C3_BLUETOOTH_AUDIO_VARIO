0e711016e6f73130e06f3052a13211af9af973b5
baskluiters <bas@kluiters.com>
2024-09-22 11:11:13 +0200

    Refactored audio
    Internalized on(0 and off().

--- src/audio.cpp
+++ src/audio.cpp
@@ -2,55 +2,57 @@
 #include "config.h"
 #include "audio.h"
 
-bool IsMuted = false;
+static bool IsMuted;
 static const uint8_t channel = 0;
 
-void audio_init(void) {
-	pinMode(pinAudioEn, OUTPUT_OPEN_DRAIN); // output enable for 74HC240, active low
-	digitalWrite(pinAudioEn, HIGH);
+static void audio_on() {
+	digitalWrite(pinAudioEn, LOW);
 	ledcWrite(channel, 2048);
 	ledcAttachPin(pinAudio, channel);
 }
 
-static void audio_tone(int freqHz, int milliseconds) {
-	digitalWrite(pinAudioEn, 0);
-	ledcWriteTone(channel, freqHz);
-	delay(milliseconds);
-	audio_off();
-	}
-
-
-void audio_off() {
-	digitalWrite(pinAudioEn, 1);
+static void audio_off() {
+	digitalWrite(pinAudioEn, HIGH);
 	ledcWrite(channel, 0);
+	ledcDetachPin(pinAudio);
 	pinMode(pinAudio, OUTPUT);
 	digitalWrite(pinAudio, 0);
-	}
+}
 
+void audio_toggle_mute() {
+	IsMuted = !IsMuted;
+	if( IsMuted )
+		audio_off();
+	else
+		audio_on();
+}
 
-void audio_beep(int freqHz, int onMs, int offMs, int numBeeps) {
+void audio_init(void) {
+	pinMode(pinAudioEn, OUTPUT_OPEN_DRAIN); // output enable for 74HC240, active low
+	IsMuted = false;
+	audio_toggle_mute();
+}
+
+void audio_generate_tone(int freqHz, int msOn, int msOff, int numBeeps) {
 	while(numBeeps--) {
-		audio_tone(freqHz, onMs);
-		delay(offMs);
-		}
+		audio_on();
+		ledcWriteTone(channel, freqHz);
+		delay(msOn);
+		audio_off();
+		if( numBeeps )
+			delay(msOff);
 	}
-
+}
 
 void audio_set_frequency(int freqHz) {
 	if (!IsMuted) {
 		if (freqHz > 0) {
-			digitalWrite(pinAudioEn, 0);
+			digitalWrite(pinAudioEn, LOW);
 			ledcWriteTone(channel, freqHz);	
-			}
+		}
 		else {
-			audio_off();
-			}
+			digitalWrite(pinAudioEn, HIGH);
+			ledcWriteTone(channel, 1);	
 		}
 	}
-
-
-void audio_generate_tone(int freqHz, int ms) {
-	digitalWrite(pinAudioEn, 0);
-	audio_tone(freqHz, ms);
-	digitalWrite(pinAudioEn, 1);
-	}
+}
